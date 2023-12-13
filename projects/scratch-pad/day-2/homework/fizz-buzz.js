8ccfc4fc988a6e0e16c37ec39f8052959b2ff42a
Sara McMullin <mcsara1221@gmail.com>
2023-12-13 02:36:36 +0000

    fizzbuzz homework complete

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,15 +12,16 @@
 */
 function fizzBuzz(num) {
   
-
-    for (let i = 1; i <= num; i++) {
+    for (let i = 1; i <= 100; i++) {
       if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
       } else if (i % 3 === 0) {
         console.log("Fizz");
       } else if (i % 5 === 0) {
         console.log("Buzz");
-      } 
+      }else{
+        console.log(i)
+      }
     }
   
     return num;
