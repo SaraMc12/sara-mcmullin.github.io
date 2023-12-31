94007c2f89751c5e2b6c3668b41e8a6b510b98c1
Sara McMullin <mcsara1221@gmail.com>
2023-12-31 15:35:49 +0000

    started studies documentation on variables

--- Studies/variables.js
+++ Studies/variables.js
@@ -6,10 +6,40 @@
  * VARIABLES
  * 
  * 0. introductory section - summary of what a variable is
+ *  variable is a named storage location that holds data or a value. 
+ * It serves as a way to identify and reference a piece of memory where information can be stored and retrieved
+ *  during the execution of a program. Variables provide a way to manipulate and work with data in a program.
+
+ Variables have the following characteristics:
  * 
- * 1. explain declaration and assignment (give code example)
+ * *1. explain declaration and assignment (give code example)
+ * 
+ * Name: A variable has a name, also known as an identifier, 
+ * which is used to uniquely identify it in the program.
+
+ *Type: Variables have a data type that defines the kind of data they can store. 
+ Common data types include numbers, strings, booleans, objects, arrays, etc.
+
+ *Value: Variables can store and hold a specific value or data. 
+ The value can be assigned, updated, and retrieved during the program's execution.
+
+ *Scope: The scope of a variable defines where in the program the variable can be accessed. 
+ Variables can have global scope (accessible throughout the entire program) or local scope 
+ (limited to a specific block or function).
  * 
  * 2. var, let, const(give code example)
- * 
- * 3. hoisting(give code example)
  */
+ var animal = "dog"
+
+// Using let (for variables that can be reassigned)
+ let animalTypes = ["mammal", "reptile", "birds"]
+
+//  Using const (for variables with constant values)
+ const animalObj = {
+    animalName: "chicken",
+    animalType: "bird",
+    favFood: "worms"
+ }
+
+//3. hoisting(give code example)
+// 
--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -48,18 +48,26 @@ return true
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
-
-function dominantDirection(text, uniCode) {
-      if ((code >= 0x0600 && code <= 0x06FF) || (code >= 0x0750 && code <= 0x077F) || (code >= 0xFB50 && code <= 0xFDFF) || (code >= 0xFE70 && code <= 0xFEFF)) {
+// I have all the directons for the text using uniode, with the return values
+// Next Write a function that computes the dominant writing direction in a string of text. 
+function dominantDirection( uniCode) {
+      if ((uniCode >= 0x0600 && uniCode <= 0x06FF) || (uniCode >= 0x0750 && uniCode <= 0x077F) || (uniCode >= 0xFB50 && uniCode <= 0xFDFF) || (uniCode >= 0xFE70 && uniCode <= 0xFEFF)) {
         return "rtl"; // Right-to-left script range
-      } else if ((code >= 0x0041 && code <= 0x007A) || (code >= 0x00C0 && code <= 0x017F) || (code >= 0x2000 && code <= 0x2CFF)) {
+      } else if ((uniCode >= 0x0041 && uniCode <= 0x007A) || (uniCode >= 0x00C0 && uniCode <= 0x017F) || (uniCode >= 0x2000 && uniCode <= 0x2CFF)) {
         return "ltr"; // Left-to-right script range
-      } else if ((code >= 0x2800 && code <= 0x28FF)) {
+      } else if ((uniCode >= 0x2800 && uniCode <= 0x28FF)) {
         return "ttb"; // Top-to-bottom script range
       } else {
         return "unknown";
       }
-    };
+      
+    const textReducer = uniCode.reduce((acc, currentValue) =>{
+      return acc += currentValue
+      }, "")
+
+        // I think this will have the map function, but the book say using reduce is a hint
+      }
+    
 
 
 console.log(dominantDirection(test))
