b5a9309d9e8f4534b096460138c85f3cd16cfae1
Sara McMullin <mcsara1221@gmail.com>
2024-01-01 17:12:13 +0000

    studies operators finished

--- Studies/operations.js
+++ Studies/operations.js
@@ -1,6 +1,33 @@
-// assignment operators
-// arithmatic operators
-// comparrison
-// logical
-// urnary
-// ternary
+// what are operators
+/**
+ * Operators are fundamental to performing various tasks in a programming language, 
+ * such as arithmetic calculations, logical comparisons, and more.
+ * Some examples of Operators are:
+ */
+/**
+ * Arithmetic Operators:
+
+Perform basic arithmetic operations like addition, subtraction, multiplication, division, and modulus.
+Example: +, -, *, /, %
+
+Comparison Operators:
+
+Compare two values and return a Boolean result (true or false).
+Example: ==, ===, !=, !==, <, >, <=, >=
+
+Logical Operators:
+
+Combine multiple conditions and return a Boolean result.
+Example: && (logical AND), || (logical OR), ! (logical NOT)
+
+Urnary Operator
+
+increase or decrease the value of a variable.
+Example: ++, --
+
+ */
+
+// Ternary (Conditional) Operator:
+
+// A shorthand way to write simple conditional statements.
+// Example: condition ? expr1 : expr2
